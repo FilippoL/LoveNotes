2dd3e3d38c72f22d650149bd2c5560a05c98adb4
FilippoLibardiTecnolife <f.libardi@tecnolife.com>
2025-12-10 17:11:02 +0100

    fix: disable unstable package exports in metro config to fix Firebase Auth registration

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,8 @@ const config = getDefaultConfig(__dirname);
 // Add support for .cjs modules (required for Firebase)
 config.resolver.sourceExts.push('cjs');
 
+// Disable experimental package exports (required to fix Firebase Auth registration error)
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
 
